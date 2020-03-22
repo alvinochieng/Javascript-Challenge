0b12b8ea99d1d8544adaef60c3d470e9dd667875
alvinochieng <56371121+alvinochieng@users.noreply.github.com>
2020-03-21 22:17:10 -0400

    finally got the for loop to work

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -12,3 +12,36 @@ tableData.forEach(function(uforeport){
         cell.text(value);
 });
 });
+
+// working on the search 
+var searchButton = d3.select("#button");
+
+searchButton.on("click", function() {
+    
+    // prevents console results from flashing for a quick second.
+    event.preventDefault();
+
+    var input  = d3.select("#example");
+
+    var inputValue = input.property("value");
+
+    var Searchfilter = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(Searchfilter);
+    
+    var searchResults = d3.select("#res");
+
+    tableBody.html("");
+
+    Searchfilter.forEach(rows => {
+        
+        tableBody.append("tr");
+
+        for (key in rows){
+
+            var cell = tableBody.append("td");
+
+            cell.text(rows[key]);
+        }
+    });
+});
